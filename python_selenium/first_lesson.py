5138d1b45e28f3626353458a492ed312c2feb3b5
HÜMEYRA ÇİMEN <75569106+hmyrcmn@users.noreply.github.com>
2023-04-13 22:56:12 +0300

    Add files via upload

---
+++ python_selenium/first_lesson.py
@@ -0,0 +1,40 @@
+print("python selenium kamp intro ")
+
+#pythpn data types
+#int : 1-3-4-
+intDataType= 12
+print(intDataType,type(intDataType))
+
+#float =0.5
+floatData=0.5
+print(floatData,type(floatData))
+
+#bool = true-false
+isTrue= True
+print(isTrue,type(isTrue))
+
+#complex = karmaşık sayılar in math
+complexData=2+3j
+print(complexData,type(complexData))
+
+
+#list ,nclude mix data types
+listData=[1,2,'h',2,2,True]
+print(listData,type(listData))
+
+#tuple include mix data types 
+tupleData=(1,2,3,True,'c')
+print(tupleData,type(tupleData))
+
+#dictionary
+dictData={
+    "fist":1,
+    "second":2
+    
+}
+print(dictData,type(dictData))
+
+#Küme (set): Elemanları benzersiz (yinelenmeyen) olan veri yapılarıdır. Süslü parantezler { } veya set() fonksiyonu ile tanımlanabilirler. 
+kumeData={1, 2, 3, 'a', 'b'}
+print(kumeData,type(kumeData))
+#None: Hiçbir değer yoktur, boşluk anlamında kullanılır. None, özel bir veri tipidir. Örnek olarak, None değeri boş bir değerdir.
